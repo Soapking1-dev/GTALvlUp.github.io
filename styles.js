1e5ec098a3bdd0c068e65c62e491572535872ef9
Soapking1-dev <tomiyaffe9@gmail.com>
2024-08-07 17:08:52 +0300

    Delete styles.js

--- styles.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('proceed-button').addEventListener('click', function() {
-    document.getElementById('home-page').classList.add('hidden');
-    document.getElementById('main-content').classList.remove('hidden');
-    document.body.style.overflow = 'auto';
-  });
